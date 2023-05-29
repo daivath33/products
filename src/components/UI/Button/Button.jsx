6442f8b5143604e43272954967b02f46e1189f0c
daiva <daivath33@gmail.com>
2023-05-29 11:47:46 +0300

    Created SocialIcons component

--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -1,7 +1,15 @@
 import "./Button.css";
 
-const Button = () => {
-  return <div></div>;
+const Button = (props) => {
+  return (
+    <button
+      className="button"
+      type={props.type || "button"}
+      onClick={props.onClick}
+    >
+      {props.title}
+    </button>
+  );
 };
 
 export default Button;
--- src/components/UI/SocialIcons/SocialIcons.jsx
+++ src/components/UI/SocialIcons/SocialIcons.jsx
@@ -1,7 +1,33 @@
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaPinterestP,
+  FaInstagram,
+  FaWhatsapp,
+} from "react-icons/fa";
 import "./SocialIcons.css";
 
 const SocialIcons = () => {
-  return <div></div>;
+  return (
+    <div class="social-links">
+      <p>Share At: </p>
+      <a href="https://www.facebook.com/">
+        <FaFacebookF />
+      </a>
+      <a href="https://twitter.com/">
+        <FaTwitter />
+      </a>
+      <a href="https://www.instagram.com/">
+        <FaInstagram />
+      </a>
+      <a href="https://www.whatsapp.com/">
+        <FaWhatsapp />
+      </a>
+      <a href="https://www.pinterest.com/">
+        <FaPinterestP />
+      </a>
+    </div>
+  );
 };
 
 export default SocialIcons;
